56c6f24b5e5934970a0981d9f3cc8b5dd729509f
XiaoYang202 <707275813@qq.com>
2022-05-26 18:33:40 +0800

    day2

---
+++ day2/changechar.java
@@ -0,0 +1,18 @@
+ public class changechar{
+// //转义字符的使用演示
+ 	public static void main(String[] args){
+		
+// // \t ：一个制表位，实现对齐的功能
+// 		 System.out.println("北\t上\t广");
+// // \n ：换行符
+// 		 System.out.println("HelloWorld\nwuhu");
+// // \\ ：一个\
+// 		System.out.println("\\HelloWorld\\");
+// // \" :一个"
+// 		System.out.println("\"HelloWorld\"");
+// // \' ：一个' \r :一个回车 
+// 		System.out.println("HelloWorld\r我会了");
+//
+	System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+ 	}
+ }
